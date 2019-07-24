e0ee12dfdd914361e33e832619c0cd6559b01735
jonas <jonasromao@gmail.com>
2019-07-23 21:02:11 -0300

    Removido system out println

--- src/main/java/br/com/sme/core/schedule/RelatorioSchedule.java
+++ src/main/java/br/com/sme/core/schedule/RelatorioSchedule.java
@@ -15,11 +15,9 @@ public class RelatorioSchedule {
 	
 	JavaMailApp javaMailApp = new JavaMailApp();
 	
-	@Schedule(hour="*", minute="00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59", second="30")
+	@Schedule(hour="*", minute="00,5,10,15,20,25,30,35,40,45,50,55", second="00")
 	void agendado() {
 		try {
-			 System.out.println("agendado pela anotacao @Schedule");
-			  
 			 
 			 relatorioFacade.gerarRelatorio();
 			 
