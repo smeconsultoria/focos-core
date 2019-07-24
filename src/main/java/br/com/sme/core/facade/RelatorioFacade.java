2fd4ca56ab409ecb03b474baa9fc1fa382295b95
jonas <jonasromao@gmail.com>
2019-07-23 21:04:06 -0300

    Adicionada logica para apenas usuarios do tipo operacionais serem incluidos no relatorio.
    refatorado codigo

--- src/main/java/br/com/sme/core/facade/RelatorioFacade.java
+++ src/main/java/br/com/sme/core/facade/RelatorioFacade.java
@@ -1,9 +1,6 @@
 package br.com.sme.core.facade;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -30,7 +27,7 @@ public class RelatorioFacade {
 
 	@Inject
 	private TesteDAO testeDAO;
-	
+
 	@Inject
 	private ConfiguracaoDAO configuracaoDAO;
 
@@ -39,150 +36,133 @@ public class RelatorioFacade {
 	public void gerarRelatorio() throws Exception {
 
 		List<Hierarquia> hierarquias = hierarquiaBusiness.getHierarquiasQueEnviamNotificacoes();
-		
+
 		JavaMailApp javaMailApp = new JavaMailApp();
-		
+
 		for (Hierarquia hierarquia : hierarquias) {
 
 			String matriculas = hierarquia.getMatriculasUsuariosReceberaoNotificacoes();
 
-			String matriculasArr[] = matriculas.split(",");
+			String matriculasArr[] = matriculas.split(";");
 
-			StringBuilder emailsUsuarios = new StringBuilder();
+			StringBuilder destinatariosRelatorio = new StringBuilder();
+			for (String matricula : matriculasArr) {
 
-			List<Usuario> usuarios = hierarquia.getUsuarios();
+				Usuario usuario = usuarioBusiness.findByMatriculaEmailIsNotNull(matricula);
 
-			logger.info("usuarios encontrados: " + usuarios.size());
+				if (usuario != null) {
 
-			StringBuilder strConteudoEmail = new StringBuilder();
-			
-			strConteudoEmail.append("<html>");
-			strConteudoEmail.append("<head>");
-			strConteudoEmail.append("<style>");
-			strConteudoEmail.append(" table {font-family: arial, sans-serif;border-collapse: collapse;width: 60%;} ");
-			strConteudoEmail.append(" td, th {border: 1px solid #a7a7a7 ;text-align: left;padding: 8px;} ");
-			strConteudoEmail.append(" tr:nth-child(even) {background-color: #dddddd;} ");
-			strConteudoEmail.append(" .even {background-color: #dddddd;} ");
-			strConteudoEmail.append(" </style> ");
-			strConteudoEmail.append(" </head> ");
-			strConteudoEmail.append(" <body> ");
-			
-			
-			String saudacao = "Prezados, <br> os seguintes colaboradores não executaram testes nas ultimas " + hierarquia.getQuantidadeHorasSemFazerTeste() + " horas.";
-			strConteudoEmail.append(saudacao);
-			
-			
-			strConteudoEmail.append(" <table>");
-			strConteudoEmail.append(" <tr><th>Nome</th><th>Matricula</th></tr> ");
-			strConteudoEmail.append("");
-			strConteudoEmail.append("");
-			
-			int contador = 2;
-			for (Usuario usuario : usuarios) {
+					destinatariosRelatorio.append(usuario.getEmail());
+					destinatariosRelatorio.append(",");
+				}
+
+			}
+
+			String htmlMail = getHtmlEmail(hierarquia);
+
+			if (!htmlMail.isEmpty()) {
+
+				try {
+
+					List<Configuracao> configuracoes = configuracaoDAO.findAll();
+
+					if (!configuracoes.isEmpty()) {
+						String remetente = configuracoes.get(0).getMailRemente();
+						String senha = configuracoes.get(0).getSenhaRemetente();
+						String assunto = "Usuários que não executaram Teste de Prontidão";
+
+						String type = "text/html";
+
+						javaMailApp.enviarEmail(assunto, htmlMail, remetente, destinatariosRelatorio.toString(), senha,
+								type);
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+
+	}
+
+	private String getHtmlEmail(Hierarquia hierarquia) {
+		StringBuilder strConteudoEmail = new StringBuilder();
+
+		strConteudoEmail.append("<html>");
+		strConteudoEmail.append("<head>");
+		strConteudoEmail.append("<style>");
+		strConteudoEmail.append(" table {font-family: arial, sans-serif;border-collapse: collapse;width: 60%;} ");
+		strConteudoEmail.append(" td, th {border: 1px solid #a7a7a7 ;text-align: left;padding: 8px;} ");
+		strConteudoEmail.append(" tr:nth-child(even) {background-color: #dddddd;} ");
+		strConteudoEmail.append(" .even {background-color: #dddddd;} ");
+		strConteudoEmail.append(" </style> ");
+		strConteudoEmail.append(" </head> ");
+		strConteudoEmail.append(" <body> ");
+
+		String saudacao = "Prezados, <br> os seguintes colaboradores não executaram testes nas ultimas "
+				+ hierarquia.getQuantidadeHorasSemFazerTeste() + " horas.";
+		strConteudoEmail.append(saudacao);
+
+		strConteudoEmail.append(" <table>");
+		strConteudoEmail.append(" <tr><th>Nome</th><th>Matricula</th></tr> ");
+
+		boolean adicionouUsuario = false;
+
+		String operacional = "O";
+
+		for (Usuario usuario : hierarquia.getUsuarios()) {
+
+			if (operacional.equals(usuario.getTipoUsuario())) {
 
 				Teste teste = testeDAO.findByLastIdUSuario(usuario.getId());
 
-				if (teste != null) {
+				if (teste == null) {
+
+					strConteudoEmail.append(generateLinhaRelatorio(usuario));
+
+					adicionouUsuario = true;
+
+				} else {
 					Calendar atual = Calendar.getInstance();
 					atual.add(Calendar.HOUR, -hierarquia.getQuantidadeHorasSemFazerTeste());
 
 					Calendar dataTeste = Calendar.getInstance();
 					dataTeste.setTime(teste.getDataExecucao());
 
-					
-					if(dataTeste.before(atual)) {
-						
-						if(contador%2 == 0) {
-							
-							strConteudoEmail.append("<tr>");
-							
-						} else {
-							
-							strConteudoEmail.append("<tr class=\"even\">");
-							
-						}
-						
-						strConteudoEmail.append("<td>");
-						strConteudoEmail.append(teste.getUsuario().getNome());
-						strConteudoEmail.append("</td>");	
-						
-						strConteudoEmail.append("<td>");
-						strConteudoEmail.append(teste.getUsuario().getMatricula());
-						strConteudoEmail.append("</td>");
-						
-						strConteudoEmail.append("</tr>");
-						
-						
+					if (dataTeste.before(atual)) {
+
+						strConteudoEmail.append(generateLinhaRelatorio(usuario));
+
+						adicionouUsuario = true;
 					}
-
 				}
-
 			}
-			
-			if(!strConteudoEmail.toString().isEmpty()) {
-				
-				strConteudoEmail.append("</table></body></html>");
-				
-				try {
-				
-					List<Configuracao> configuracoes = configuracaoDAO.findAll();
-					
-					if(!configuracoes.isEmpty()) {
-						String remetente = configuracoes.get(0).getMailRemente();
-						String senha = configuracoes.get(0).getSenhaRemetente();
-						String assunto = "Usuários que não executaram Teste de Prontidão";
-						
-						String type = "text/html";
-						
-						javaMailApp.enviarEmail(assunto, strConteudoEmail.toString(), remetente, senha, type);
-					}
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-				
-				
-				
-			}
-			
-
-			/*
-			 * for (String matricula : matriculasArr) {
-			 * 
-			 * Usuario usuario = usuarioBusiness.findByMatriculaEmailIsNotNull(matricula);
-			 * 
-			 * if (usuario != null) {
-			 * 
-			 * emailsUsuarios.append(usuario.getEmail()); }
-			 * 
-			 * }
-			 * 
-			 * System.out.println(emailsUsuarios);
-			 */
 
 		}
 
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		try {
-			Calendar atual = Calendar.getInstance();
-			atual.add(Calendar.HOUR, -48);
+		strConteudoEmail.append("</table></body></html>");
 
-			Calendar dataTeste = Calendar.getInstance();
-			SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-			Date date1 = formatter1.parse("17/07/2019 10:00:00");
-			dataTeste.setTime(date1);
-
-			System.out.println(dataTeste.before(atual));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
-		
-		
+		return adicionouUsuario ? strConteudoEmail.toString() : "";
 	}
+
+	private String generateLinhaRelatorio(Usuario usuario) {
+		StringBuilder linhaRelatorio = new StringBuilder();
+
+		linhaRelatorio.append("<tr>");
+
+		linhaRelatorio.append("<td>");
+		linhaRelatorio.append(usuario.getNome());
+		linhaRelatorio.append("</td>");
+
+		linhaRelatorio.append("<td>");
+		linhaRelatorio.append(usuario.getMatricula());
+		linhaRelatorio.append("</td>");
+
+		linhaRelatorio.append("</tr>");
+
+		return linhaRelatorio.toString();
+	}
+
 }
